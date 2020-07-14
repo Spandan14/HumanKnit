168afaf60230e472be3a516441f65a5c8efcc176
Frank Gao <frank@swoag.com>
2020-07-14 12:07:59 -0500

    fixed colors and added drop shadow to buttons

--- humanknit/lib/experiences.dart
+++ humanknit/lib/experiences.dart
@@ -11,9 +11,13 @@ class ExperiencesPage extends StatelessWidget {
       textAlign: TextAlign.center,
       style: TextStyle(
         fontSize: 48 / 896 * screenHeight,
+        color: Color(0xff7348a6),
       ),
     );
-    print(screenHeight);
+
+    final volunteeringButton = makeButton("Volunteering", "Lend a helping hand", screenHeight);
+    final votingButton = makeButton("Voting", "Make your voice heard", screenHeight);
+    final communityButton = makeButton("Community Events", "Enjoy the fun!", screenHeight);
 
     return MaterialApp(
       home: Scaffold(
@@ -21,9 +25,9 @@ class ExperiencesPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             pageTitle,
-            makeButton("Volunteering", "Lend a helping hand", screenHeight),
-            makeButton("Voting", "Make your voice heard", screenHeight),
-            makeButton("Community Events", "Enjoy the fun!", screenHeight),
+            dropShadowContainer(volunteeringButton),
+            dropShadowContainer(votingButton),
+            dropShadowContainer(communityButton),
           ],
         ),
         bottomNavigationBar: BottomNavigationBar(
@@ -67,26 +71,50 @@ class ExperiencesPage extends StatelessWidget {
     );
   }
 
+  Container dropShadowContainer(Widget child) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30),
+        boxShadow: [
+          BoxShadow(
+            color: Color(0xff000000),
+            blurRadius: 10,
+            offset: Offset(
+              0,
+              4,
+            ),
+          ),
+        ],
+      ),
+      child: child,
+    );
+  }
+
   FlatButton makeButton(String title, String description, double screenHeight) {
     return FlatButton(
+      color: Color(0xff35c38d),
+      padding: EdgeInsets.all(20),
+      onPressed: () {},
       child: Column(
         children: [
           Text(
             title,
             style: TextStyle(
-              fontSize: 30/896 * screenHeight,
+              fontSize: 30 / 896 * screenHeight,
+              color: Color(0xfff25740),
             ),
           ),
           Text(
             description,
             style: TextStyle(
-              fontSize: 20/896 * screenHeight,
+              fontSize: 20 / 896 * screenHeight,
+              color: Color(0xff000000),
             ),
           ),
         ],
       ),
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20.0),
+        borderRadius: BorderRadius.circular(30.0),
       ),
     );
   }
