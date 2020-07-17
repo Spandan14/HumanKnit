00a4974795dc378b6cbf3fa55f0d03ca227f9014
Spandan Goel <spagoel22@students.d125.org>
2020-07-17 13:03:13 -0500

    screwed up firebase

--- humanknit/lib/settings.dart
+++ humanknit/lib/settings.dart
@@ -3,8 +3,42 @@ import 'package:flutter/material.dart';
 class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Settings Page"),
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
+    return MaterialApp (
+      theme: ThemeData(fontFamily: 'BungeeInline'),
+      home: Column (
+        children: <Widget>[
+          Container(
+            child: Padding (
+              padding: EdgeInsets.only(top: 40, left: 0.15 * width, right: 0.15 * width, bottom: 40),
+              child: Material(
+                child: Text (
+                  "Settings",
+                  style: TextStyle (
+                    fontFamily: 'BungeeInline',
+                    fontSize: 24
+                  ),
+                )
+              ),
+            )
+          ),
+          SettingsDropdowns()
+        ],
+      )
     );
+  }
+}
+
+class SettingsDropdowns extends StatefulWidget {
+  @override
+  _SettingsDropdownsState createState() => _SettingsDropdownsState();
+}
+
+class _SettingsDropdownsState extends State<SettingsDropdowns> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
