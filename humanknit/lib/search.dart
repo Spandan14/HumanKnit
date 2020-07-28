32c6f42dc4bb9823c7a749769169c99678fd07b8
Frank Gao <frank@swoag.com>
2020-07-28 16:44:00 -0500

    implemented events near you page basic layout with html scraping(no back button lol)

---
+++ humanknit/lib/search.dart
@@ -0,0 +1,262 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+enum EVENT_TYPE {
+  VOLUNTEER,
+  VOTE,
+  COMMUNITY,
+}
+
+class SearchPage extends StatefulWidget {
+  EVENT_TYPE type;
+
+  @override
+  State<StatefulWidget> createState() {
+    return SearchPageState(type);
+  }
+
+  SearchPage(EVENT_TYPE t) {
+    type = t;
+  }
+}
+
+class SearchPageState extends State<SearchPage> {
+  EVENT_TYPE type;
+  var title;
+  final flutterWebviewPlugin = new FlutterWebviewPlugin();
+  List<Widget> children;
+  var closed = false;
+  double screenHeight = 0;
+  double screenWidth = 0;
+
+  @override
+  void dispose() {
+    flutterWebviewPlugin.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    screenHeight = MediaQuery.of(context).size.height;
+    screenWidth = MediaQuery.of(context).size.width;
+
+    final titleText = Text(
+      title,
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        fontSize: 48 / 896 * screenHeight,
+        color: Color(0xfffcba03),
+      ),
+    );
+    final nearYouText = Text(
+      "Near you:",
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        fontSize: 42 / 896 * screenHeight,
+        color: Color(0xfffcba03),
+      ),
+    );
+
+    final searchBar = TextField(
+      style: TextStyle(
+        color: Color(0xffb1b1b1),
+      ),
+      decoration: InputDecoration(
+        fillColor: Color(0xffffffff),
+        filled: true,
+        hintText: "Search",
+        hintStyle: TextStyle(
+          fontSize: 24 / 896 * screenHeight,
+          color: Color(0xffb1b1b1),
+        ),
+        prefixIcon: Transform.scale(
+          scale: 0.5,
+          child: ImageIcon(
+            AssetImage("assets/images/search.png"),
+          ),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+          borderSide: BorderSide(color: Color(0xffb1b1b1)),
+        ),
+      ),
+    );
+
+    getTitleColumn();
+
+    return Scaffold(
+      backgroundColor: Color(0xff6c7bff),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Flexible(
+            child: titleText,
+          ),
+          Flexible(
+            child: nearYouText,
+          ),
+          Flexible(
+            flex: 3,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                color: Colors.transparent,
+              ),
+              child: ListView(
+                children: children ??
+                    [
+                      Center(
+                        child: SizedBox(
+                          width: 45 / 896 * screenHeight,
+                          height: 45 / 896 * screenHeight,
+                          child: CircularProgressIndicator(),
+                        ),
+                      ),
+                    ],
+                shrinkWrap: true,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  SearchPageState(EVENT_TYPE t) {
+    type = t;
+    switch (type) {
+      case EVENT_TYPE.VOLUNTEER:
+        title = "Volunteering Events";
+        break;
+      case EVENT_TYPE.VOTE:
+        title = "Voting Events";
+        break;
+      case EVENT_TYPE.COMMUNITY:
+        title = "Community Events";
+        break;
+    }
+  }
+
+  void getTitleColumn() {
+    if (closed) {
+      return;
+    }
+
+    StringBuffer buffer =
+        new StringBuffer("https://www.volunteermatch.org/search/");
+    buffer.write("l=" + Uri.encodeComponent("Long+Grove%2C+IL%2C+USA"));
+
+    flutterWebviewPlugin.launch(buffer.toString(), hidden: true);
+    flutterWebviewPlugin.onProgressChanged.listen((progress) async {
+      if (progress == 1) {
+        flutterWebviewPlugin
+            .evalJavascript("document.documentElement.outerHTML")
+            .then((html) {
+          final titles = getStrings(html, ". </span>", "\n");
+          final locations = getStrings(html,
+              "saddr=Current%20Location&amp;daddr=", "\" target=\"_blank\">");
+          final dates =
+              getStrings(html, "<span class=\"opp_ongoing\">", "</span>");
+
+          children = List<Widget>();
+          for (int i = 0; i < titles.length; i++) {
+            children.add(
+              Padding(
+                padding: EdgeInsets.only(
+                    left: screenWidth / 12,
+                    right: screenWidth / 12,
+                    top: screenWidth / 48,
+                    bottom: screenWidth / 48),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color(0xffffffff),
+                    border: Border.all(
+                      color: Color(0xff000000),
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(screenWidth / 12),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(
+                          titles[i],
+                          textAlign: TextAlign.center,
+                          maxLines: 5,
+                          style: TextStyle(
+                            fontSize: 30 / 896 * screenHeight,
+                          ),
+                        ),
+                        SizedBox(
+                          height: screenWidth / 6,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Flexible(
+                              child: Image.asset("assets/images/location.png"),
+                            ),
+                            Flexible(
+                              flex: 5,
+                              child: Text(
+                                locations[i],
+                                textAlign: TextAlign.center,
+                                maxLines: 5,
+                                style: TextStyle(
+                                  fontSize: 22 / 896 * screenHeight,
+                                  color: Color(0xff6c7bff),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: screenWidth / 6,
+                        ),
+                        RaisedButton(
+                          color: Color(0xfffcba03),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(1000),
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.all(screenWidth / 16),
+                            child: Text(
+                              "Details >",
+                              style: TextStyle(
+                                fontSize: 36 / 896 * screenHeight,
+                              ),
+                            ),
+                          ),
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            );
+          }
+          flutterWebviewPlugin.close();
+          closed = true;
+          setState(() {});
+        });
+      }
+    });
+  }
+
+  List<String> getStrings(String html, String sStr, String eStr) {
+    final startStr = sStr;
+    final endStr = eStr;
+    var titles = List<String>();
+
+    while (html.indexOf(startStr) != -1) {
+      final startIndex = html.indexOf(startStr);
+      final endIndex = html.indexOf(endStr, startIndex + startStr.length);
+      titles.add(html.substring(startIndex + startStr.length, endIndex));
+      html = html.substring(endIndex, html.length);
+    }
+    return titles;
+  }
+}
