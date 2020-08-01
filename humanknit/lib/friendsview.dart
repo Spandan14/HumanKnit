bc6603d3ae6dd23226f2fc68ca80527008d2b16b
Spandan Goel <spagoel22@students.d125.org>
2020-07-31 22:58:51 -0500

    finished ui for friends and requests, acceptance now possible

--- humanknit/lib/friendsview.dart
+++ humanknit/lib/friendsview.dart
@@ -1,4 +1,5 @@
 import 'dart:html';
+import 'dart:ui';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
@@ -13,10 +14,15 @@ class FriendListPage extends StatefulWidget {
 
 class _FriendListPageState extends State<FriendListPage> {
 
-  List<String> _friendUserPFPS = List<String>();
-  List<String> _friendUserNames = List<String>();
-  List<String> _friendUserUsernames = List<String>();
-  List<String> _friendUserUIDS = List<String>();
+
+
+  static ScrollController _scrollControlFriend = new ScrollController();
+  static ScrollController _scrollControlRequest = new ScrollController();
+
+  static List<String> _friendUserPFPS = List<String>();
+  static List<String> _friendUserNames = List<String>();
+  static List<String> _friendUserUsernames = List<String>();
+  static List<String> _friendUserUIDS = List<String>();
 
   Future<void>fetchFriendsData() async {
     setState(() {
@@ -33,14 +39,32 @@ class _FriendListPageState extends State<FriendListPage> {
     List<String> friendUserNames = List<String>();
     List<String> friendUserUsernames = List<String>();
     List<String> friendUserUIDS = List<String>();
-    
+    await friendDocs.forEach((friend) {
+      String currentfriendUID = friend.data["friend"];
+      DocumentReference friendUser = Firestore.instance.document("users/$currentfriendUID/data/profileData");
+      DocumentReference friendUserTwo = Firestore.instance.document("users/$currentfriendUID");
+      friendUser.get().then((datasnapshot) {
+        friendUserPFPS.add(datasnapshot.data["pic"]);
+        friendUserNames.add(datasnapshot.data["name"]);
+      });
+      friendUserTwo.get().then((datasnapshot) {
+        friendUserUsernames.add(datasnapshot.data["name"]);
+      });
+      friendUserUIDS.add(currentfriendUID);
+    });
+    setState(() {
+      _friendUserNames = friendUserNames;
+      _friendUserPFPS = friendUserPFPS;
+      _friendUserUIDS = friendUserUIDS;
+      _friendUserUsernames = friendUserUsernames;
+    });
   }
 
 
-  List<String> _requestUserPFPS = List<String>();
-  List<String> _requestUserNames = List<String>();
-  List<String> _requestUserUsernames = List<String>();
-  List<String> _requestUserUIDS = List<String>();
+    static List<String> _requestUserPFPS = List<String>();
+    static List<String> _requestUserNames = List<String>();
+    static List<String> _requestUserUsernames = List<String>();
+    static List<String> _requestUserUIDS = List<String>();
 
   Future<void>fetchRequestsData() async {
     setState(() {
@@ -78,14 +102,262 @@ class _FriendListPageState extends State<FriendListPage> {
     });
   }
 
-  Scaffold friends;
-  Scaffold requests;
-  Scaffold page;
+
+  bool buttonVis = true;
+  acceptFriend(String requesterUID) async {
+    FirebaseUser user = await FirebaseAuth.instance.currentUser();
+    String uid = user.uid;
+    DocumentReference request = await Firestore.instance.document("users/$uid/data/friendsData/requests/$requesterUID");
+    await request.delete();
+    Firestore.instance.document("users/$uid/data/friendsData/friends/$requesterUID}").setData({
+      "friend": requesterUID,
+    }, merge: true);
+    Firestore.instance.document("users/$requesterUID}/data/friendsData/friends/$uid").setData({
+      "friend": uid,
+    }, merge: true);
+    setState(() {
+      buttonVis = false;
+    });
+  }
+
+  rejectFriend(String requesterUID) async {
+    FirebaseUser user = await FirebaseAuth.instance.currentUser();
+    String uid = user.uid;
+    DocumentReference request = await Firestore.instance.document("users/$uid/data/friendsData/requests/$requesterUID");
+    await request.delete();
+    setState(() {
+      buttonVis = false;
+    });
+  }
+
+  getTileRequest(int position) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    String username = _requestUserUsernames[position];
+    String name = _requestUserNames[position];
+    String pfp = _requestUserPFPS[position];
+    String uid = _requestUserUIDS[position];
+    return Container(
+      child: Card(
+        child: ListTile(
+          contentPadding: EdgeInsets.symmetric(vertical: 8/692 * height, horizontal: 6/360 * width),
+          leading: Row(
+            children: <Widget>[
+              Container(
+                height: 60,
+                width: 60,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: NetworkImage(pfp),
+                        fit: BoxFit.cover
+                    ),
+                    border: Border.all(
+                        color: Colors.black,
+                        width: 2
+                    ),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(500),
+                    )
+                ),
+              ),
+              Visibility(
+                visible: buttonVis,
+                child: Row(
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(
+                        Icons.check,
+                        color: Colors.green,
+                      ),
+                      tooltip: "Accept Friend Request",
+                      onPressed: () {
+                        acceptFriend(uid);
+                      },
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        Icons.close,
+                        color: Colors.red,
+                      ),
+                      tooltip: "Reject Friend Request",
+                      onPressed: () {
+                        rejectFriend(uid);
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              Visibility(
+                visible: !buttonVis,
+                child: Icon(
+                  Icons.check_circle_outline,
+                  color: Colors.green,
+                  size: 60,
+                ),
+              )
+
+            ],
+          ),
+          title: Text(
+            name,
+          ),
+          subtitle: Text(
+            username,
+          ),
+        ),
+      ),
+    );
+  }
+
+
+
+
+  getTileFriend(int position) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    String username = _friendUserUsernames[position];
+    String name = _friendUserNames[position];
+    String pfp = _friendUserPFPS[position];
+    String uid = _friendUserUIDS[position];
+    return Container(
+      child: Card(
+        child: ListTile(
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) => FriendProfilePage(friendUID: uid,)));
+          },
+          contentPadding: EdgeInsets.symmetric(vertical: 8/692 * height, horizontal: 6/360 * width),
+          leading: Container(
+            height: 60,
+            width: 60,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage(pfp),
+                    fit: BoxFit.cover
+                ),
+                border: Border.all(
+                    color: Colors.black,
+                    width: 2
+                ),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(500),
+                )
+            ),
+          ),
+          title: Text(
+            name,
+          ),
+          subtitle: Text(
+            username,
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
     print("friendspage");
+
+    List<bool> isSelected = [true,false];
+    Scaffold page, friends, requests;
+
+    friends = Scaffold(
+      resizeToAvoidBottomInset: true,
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            AppBar(
+              backgroundColor: Colors.black,
+              actions: <Widget>[
+              ],
+            ),
+            ToggleButtons(
+              children: <Widget>[
+                Icon(Icons.person_outline),
+                Icon(Icons.person_add),
+              ],
+              onPressed: (int index) {
+                setState(() {
+                  if (index == 0) {
+                    page = friends;
+                  }
+                  if (index == 1) {
+                    page = requests;
+                  }
+                });
+              },
+              isSelected: isSelected,
+            ),
+            SingleChildScrollView(
+              child: Container(
+                height: 430,
+                child: ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  controller: _scrollControlFriend,
+                  shrinkWrap: true,
+                  itemBuilder: (BuildContext context, index) {
+                    return(getTileFriend(index));
+                  },
+                  itemCount: _friendUserUsernames.length,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+
+    requests = Scaffold(
+      resizeToAvoidBottomInset: true,
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            AppBar(
+              backgroundColor: Colors.black,
+              actions: <Widget>[
+              ],
+            ),
+            ToggleButtons(
+              children: <Widget>[
+                Icon(Icons.person_outline),
+                Icon(Icons.person_add),
+              ],
+              onPressed: (int index) {
+                setState(() {
+                  if (index == 0) {
+                    page = friends;
+                  }
+                  if (index == 1) {
+                    page = requests;
+                  }
+                });
+              },
+              isSelected: isSelected,
+            ),
+            SingleChildScrollView(
+              child: Container(
+                height: 430,
+                child: ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  controller: _scrollControlRequest,
+                  shrinkWrap: true,
+                  itemBuilder: (BuildContext context, index) {
+                    return(getTileRequest(index));
+                  },
+                  itemCount: _requestUserUsernames.length,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+
+
+
+
     try {
       return page;
     } catch (e) {
